4958986df9b3847dc576a5739fe18319a0abefc7
Eric Bower <neurosnap@gmail.com>
2017-12-23 17:28:00 -0500

    folder reorg

---
+++ packages/yaplint/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="yaplint",
+    version="1.0.0",
+    description="yet another python linter",
+    install_requires=[],
+    author='Eric Bower',
+    author_email='neurosnap@gmail.com',
+    url='https://github.com/neurosnap/yaplint',
+    py_modules=["yaplint"],
+)
